53d6cbdd7394e7954d38b744ce3b12a0ffae64f8
Yulian Karapetkov <yulian.karapetkov@gmail.com>
2018-06-23 16:47:16 +0300

    added todo item component

---
+++ src/app/todos/shared/components/todo-item/todo-item.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'todos-todo-item',
+    templateUrl: './todo-item.component.html',
+    styleUrls: ['./todo-item.component.scss']
+})
+export class TodoItemComponent implements OnInit {
+    constructor() { }
+
+    ngOnInit() {
+    }
+}
